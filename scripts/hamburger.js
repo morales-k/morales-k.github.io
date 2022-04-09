79f1f8c74f55b2e696dfd9659c29a44a134a985b
morales-k <98136783+morales-k@users.noreply.github.com>
2022-04-09 16:26:17 -0400

    Create build branch

---
+++ scripts/hamburger.js
@@ -0,0 +1,1 @@
+function onClickMenu(){document.getElementById("menu").classList.toggle("change"),document.getElementById("navigation").classList.toggle("change"),document.getElementById("menu-bg").classList.toggle("change-bg")}const menu=document.querySelector("#menu");menu.addEventListener("click",function(){onClickMenu()});
