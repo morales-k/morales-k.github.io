820979173e97e6ffcdf0ee8fc3bdc52e78a44cbb
morales-k <98136783+morales-k@users.noreply.github.com>
2022-04-09 17:09:13 -0400

    Clean up branch for new design structure

--- scripts/hamburger.js
+++
@@ -1,1 +0,0 @@
-function onClickMenu(){document.getElementById("menu").classList.toggle("change"),document.getElementById("navigation").classList.toggle("change"),document.getElementById("menu-bg").classList.toggle("change-bg")}const menu=document.querySelector("#menu");menu.addEventListener("click",function(){onClickMenu()});
