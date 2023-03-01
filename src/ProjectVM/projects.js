e6385a5f3a07305144c7fe73d4da23622b24ddb1
Krista M <morales-k@outlook.com>
2023-03-01 17:25:42 -0500

    ProjectVM
    Added view model to store project info as objects.

---
+++ src/ProjectVM/projects.js
@@ -0,0 +1,60 @@
+import memoryMatchImg from "../assets/work/projectmemory.png";
+import cactusCoImg from "../assets/work/projectcactusco.png";
+import colorTheoryImg from "../assets/work/projectcolortheory.png";
+import todoImg from '../assets/work/projecttodo.png';
+import calcImg from '../assets/work/projectcalc.png';
+import brickBreakImg from '../assets/work/projectbrickbreak.png';
+
+export const memoryMatch = {
+    title: "Memory Match",
+    imgSrc: memoryMatchImg,
+    imgAlt: "Memory match game.",
+    description: "Coffee and donut themed matching game built with React and CSS. The Fischer Yates shuffle is used so no two play throughs are the same.",
+    codeLink: "https://github.com/morales-k/Memory-Match",
+    projectLink: "https://morales-k.github.io/Memory-Match/"
+};
+
+export const cactusCo = {
+    title: "Cactus Co.",
+    imgSrc: cactusCoImg,
+    imgAlt: "Modern website selling succulents and cacti.",
+    description: "A modern website selling succulents and cacti. Featuring a dynamic shopping cart and form validation.",
+    codeLink: "https://github.com/morales-k/CactusCo",
+    projectLink: "https://morales-k.github.io/CactusCo/"
+};
+
+export const colorTheroy = {
+    title: "Color Theory",
+    imgSrc: colorTheoryImg,
+    imgAlt: "A color theroy game.",
+    description: "A color theory game using React and a responsive canvas. Select color pairs to mix and create new colors.",
+    codeLink: "https://github.com/morales-k/Color-Theory",
+    projectLink: "https://morales-k.github.io/Color-Theory/"
+};
+
+export const brickBreak = {
+    title: "Brick Break",
+    imgSrc: brickBreakImg,
+    imgAlt: "A brick breaking game.",
+    description: "A game using React and a responsive canvas. Destroy bricks. Don't let the ball fall!",
+    codeLink: "https://github.com/morales-k/BrickBreak",
+    projectLink: "https://morales-k.github.io/BrickBreak/"
+};
+
+export const todoList = {
+    title: "To-do List",
+    imgSrc: todoImg,
+    imgAlt: "To-do list app.",
+    description: "Create, delete and mark items as complete with this to-do list. Keyboard control is implemented for better access. Built with React and CSS.",
+    codeLink: "https://github.com/morales-k/To-Do-List",
+    projectLink: "https://morales-k.github.io/To-Do-List/"
+};
+
+export const calculator = {
+    title: "Classic Calculator",
+    imgSrc: calcImg,
+    imgAlt: "A calculator built with JavaScript.",
+    description: "A classic calculator with all the basics. Square roots, decimals and negative numbers included! Built using JavaScript and CSS.",
+    codeLink: "https://github.com/morales-k/Calculator",
+    projectLink: "https://morales-k.github.io/Calculator/"
+};
