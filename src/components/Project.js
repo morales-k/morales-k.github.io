cf653032511f98a86e987b1a33cb485045ddce74
Krista M <morales-k@outlook.com>
2023-03-02 14:14:43 -0500

    Added handleDisplay
    Clicking project now shows close button if not using a mouse.

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,19 +3,34 @@ import React, { useState } from 'react';
 const Project = (props) => {
     const { project } = props;
     const [showOverlay, setOverlay] = useState(false);
+    const [showClose, setShowClose] = useState(true);
+
+    const handleDisplay = (e, bool) => {
+        if (showClose && 
+            (e.type === "mouseenter" || 
+             e.type === "mouseleave")) {
+            setShowClose(false);
+        } else {
+            setShowClose(true);
+        }
+        setOverlay(bool);
+    };
 
     return (
         <div className="project"
-             onMouseEnter={() => setOverlay(true)}
-             onMouseLeave={() => setOverlay(false)}
-             onTouchStart={() => setOverlay(true)}>
+             onMouseEnter={e => handleDisplay(e, true)}
+             onMouseLeave={e => handleDisplay(e, false)}
+             onTouchStart={e => handleDisplay(e, true)}>
             {
               showOverlay ? 
               <div className="overlay">
-                  <span 
+                  {
+                    showClose ? 
+                    <span 
                       className="close"
-                      onClick={() => setOverlay(false)}
-                      onTouchStart={() => setOverlay(false)}>X</span>
+                      onClick={e => handleDisplay(e, false)}
+                      onTouchStart={e => handleDisplay(e, false)}>X</span> : null
+                  }
                   <p className="title">{project.title}</p>
                   <p>{project.description}</p>
                   <div className="view-links">
